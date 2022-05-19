b55045059d6956e8512e56b45385b6fb132e18aa
alan-cavalcante <aocaval@gmail.com>
2022-05-18 21:45:47 -0300

    Tailwind + Laravel + configs

---
+++ laravel/resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link href="../css/app.css" rel="stylesheet">
+
+    <!-- Styles -->
+</head>
+
+<body class="text-center">
+    <div class="flex flex-col bg-slate-400 p-2">
+        <h1 class=" text-3xl font-bold">
+            Testando Laravel + Tailwind!!!
+        </h1>
+    </div>
+        <p class="text-3xl">Deu certo!</p>
+</body>
+
+</html>
