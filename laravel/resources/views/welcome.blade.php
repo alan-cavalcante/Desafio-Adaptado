145972f3359790103b5a821cf7448cbd5766861b
alan-cavalcante <aocaval@gmail.com>
2022-05-30 21:47:38 -0300

    responsividade nav

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -17,95 +17,137 @@
         function abrirHamburger() {
             let x = document.getElementById("linksNav")
             x.classList.toggle('hidden');
-            // for (let i = 0; i <= x.classList.length; i++) {
-            //     if (x.classList[i] == "hidden") {
-            //         x.classList.remove("hidden")
-            //         x.classList.add("flex")
-
-            //     } else if (x.classList[i] == "flex")  {
-            //         x.classList.remove("flex")
-            //         x.classList.add("hidden")
-            //     }
-            // }
         }
 
         function abrirFeatures() {
             let y = document.getElementById("features")
+            let yy = document.getElementById("setaFeatures")
+            let down = "../img/icon-arrow-down.svg"
+            let up = "../img/icon-arrow-up.svg"
+            if (yy.attributes[0].value == down) {
+                yy.attributes[0].value = up
+            } else if(yy.attributes[0].value == up) {
+                yy.attributes[0].value = down
+            }
             y.classList.toggle('hidden')
         }
+
         function abrirCompany() {
             let z = document.getElementById("company")
+            let zz = document.getElementById("setaCompany")
+            let down = "../img/icon-arrow-down.svg"
+            let up = "../img/icon-arrow-up.svg"
+            if (zz.attributes[0].value == down) {
+                zz.attributes[0].value = up
+            } else if(zz.attributes[0].value == up) {
+                zz.attributes[0].value = down
+            }
+
             z.classList.toggle('hidden')
         }
     </script>
 </head>
 
 <body>
-
+    <!-- HEADER NAV & LOGIN -->
     <header class="flex">
         <nav class="flex justify-between w-full p-3">
-            <div class="flex items-center min-w-max m-2 sm:mx-4">
+            <!-- LOGO  -->
+            <div class="flex items-center min-w-max m-2 sm:mx-4 md:mr-16">
                 <a href="/">
                     <img src="../img/logo.svg" class="object-contain" alt="logo">
                 </a>
             </div>
-
-            <svg xmlns="http://www.w3.org/2000/svg" onclick="abrirHamburger()" class="h-6 w-6 z-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <!-- BOTÃO ABRIR MENU - MOBILE -->
+            <svg xmlns="http://www.w3.org/2000/svg" onclick="abrirHamburger()" class="h-6 w-6 z-10 md:hidden " fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
             </svg>
+            <!-- NAVEGAÇÃO -->
+            <div class="bg-white absolute right-0 top-0 flex-col h-full p-4 w-2/3 items-end drop-shadow-[0_22px_70px_rgba(0,0,0,0.56)] hidden 
+             md:static md:flex md:flex-row md:p-2 md:w-full md:items-center md:justify-between md:filter-none " id="linksNav">
 
-            <div class=" bg-white absolute right-0 top-0 flex-col h-full p-4 w-2/3 items-end hidden" id="linksNav">
-
-                <ul class="self-start flex flex-col">
-                    <button class="m-1 my-3 flex items-baseline font-projeto font-bold text-mediumgray hover:text-almostblack" onclick="abrirFeatures()"> Features
-                        <img src="../img/icon-arrow-down.svg" alt="arrow" class="ml-4">
+                <ul class="flex flex-col mt-10 md:flex-row md:mt-0 ">
+                    <button class="m-1 my-3 flex items-baseline font-projeto font-bold text-mediumgray hover:text-almostblack md:ml-4" onclick="abrirFeatures()"> Features
+                        <img src="../img/icon-arrow-down.svg" alt="arrow" class="ml-4 md:ml-2" id="setaFeatures" >
                     </button>
-                    <div id="features" class="hidden">
-                        <button class="m-1 my-3 pl-2 flex items-start font-projeto text-mediumgray hover:text-almostblack ">
-                            <img src="../img/icon-todo.svg" class="mx-2" alt="todo list" class="ml-4">
-                            Todo List
+                    
+                    <!-- JANELA FEATURES -->
+                    <div id="features" class="hidden bg-almostwhite 
+                    md:absolute md:top-20 md:left-36 md:rounded-lg md:bg-white md:drop-shadow-2xl md:p-2 md:border-2 md:border-almostwhite ">
+
+                        <button class="m-1 my-3 pl-2 flex items-baseline font-projeto text-mediumgray hover:text-almostblack
+                        md:mx-2 md:pl-0 md:my-3 text-left ">
+                            <div class=" w-8 flex flex-col items-center justify-center">
+                                <img src="../img/icon-todo.svg" class=" mx-2" alt="todo list" class="ml-4">
+                            </div>
+                            <p>Todo List</p>
                         </button>
-                        <button class="m-1 my-3 pl-2 flex items-start font-projeto text-mediumgray hover:text-almostblack ">
-                            <img src="../img/icon-calendar.svg" class="mx-2" alt="calendar" class="ml-4">Calendar
+
+                        <button class="m-1 my-3 pl-2 flex items-baseline font-projeto text-mediumgray hover:text-almostblack
+                        md:mx-2 md:pl-0 md:my-3 text-left ">
+                            <div class=" w-8 flex flex-col items-center justify-center">
+                                <img src="../img/icon-calendar.svg" class=" mx-2" alt="calendar" class="ml-4">
+                            </div>
+                            <p>Calendar</p>
                         </button>
-                        <button class="m-1 my-3 pl-2 flex items-start font-projeto text-mediumgray hover:text-almostblack ">
-                            <img src="../img/icon-reminders.svg" class="mx-2" alt="reminders" class="ml-4">Reminders
+
+                        <button class="m-1 my-3 pl-2 flex items-baseline font-projeto text-mediumgray hover:text-almostblack
+                        md:mx-2 md:pl-0 md:my-3 text-left ">
+                            <div class=" w-8 flex flex-col items-center justify-center">
+                                <img src="../img/icon-reminders.svg" class=" mx-2" alt="reminders" class="ml-4">
+                            </div>
+                            <p>Reminders</p>
                         </button>
-                        <button class="m-1 my-3 pl-2 flex items-start font-projeto text-mediumgray hover:text-almostblack ">
-                            <img src="../img/icon-planning.svg" class="mx-2" alt="planning" class="ml-4">Planning
+
+                        <button class="m-1 my-3 pl-2 flex items-baseline font-projeto text-mediumgray hover:text-almostblack
+                        md:mx-2 md:pl-0 md:my-3 text-left ">
+                            <div class=" w-8 flex flex-col items-center justify-center">
+                                <img src="../img/icon-planning.svg" class=" mx-2" alt="planning" class="ml-4">
+                            </div>
+                            <p>Planning</p>
                         </button>
                     </div>
 
-                    <button class="m-1 my-3 flex items-baseline font-projeto font-bold text-mediumgray hover:text-almostblack" onclick="abrirCompany()">Company
-                        <img src="../img/icon-arrow-down.svg" alt="arrow" class="ml-4">
+                    <button class="m-1 my-3 flex items-baseline font-projeto font-bold text-mediumgray hover:text-almostblack md:ml-4" onclick="abrirCompany()">Company
+                        <img src="../img/icon-arrow-down.svg" alt="arrow" id="setaCompany" class="ml-4 md:ml-2">
                     </button>
-
-                    <div id="company" class="hidden">
-                        <button class=" pl-2 m-1 my-3 ml-2 flex items-start font-projeto text-mediumgray hover:text-almostblack ">
+                    
+                    <!-- JANELA COMPANY -->
+                    <div id="company" class="hidden bg-almostwhite
+                    md:absolute md:top-20 md:left-80 md:rounded-lg md:bg-white md:drop-shadow-2xl md:p-2 md:border-2 md:border-almostwhite">
+                        <button class=" pl-2 m-1 my-3 ml-2 flex items-start font-projeto text-mediumgray hover:text-almostblack 
+                        md:mx-4 md:pl-0 md:my-3 text-left ">
                             History
                         </button>
-                        <button class=" pl-2 m-1 my-3 ml-2 flex items-start font-projeto text-mediumgray hover:text-almostblack ">
+                        <button class=" pl-2 m-1 my-3 ml-2 flex items-start font-projeto text-mediumgray hover:text-almostblack
+                        md:mx-4 md:pl-0 md:my-3 text-left ">
                             Our Team
                         </button>
-                        <button class=" pl-2 m-1 my-3 ml-2 flex items-start font-projeto text-mediumgray hover:text-almostblack ">
+                        <button class=" pl-2 m-1 my-3 ml-2 flex items-start font-projeto text-mediumgray hover:text-almostblack
+                        md:mx-4 md:pl-0 md:my-3 text-left ">
                             Blog
                         </button>
                     </div>
 
-                    <button class="m-1 my-3 flex items-center font-projeto font-bold text-mediumgray hover:text-almostblack ">Carreers</button>
-
-                    <button class="m-1 my-3 flex items-center font-projeto font-bold text-mediumgray hover:text-almostblack ">About</button>
+                    <button class="m-1 my-3 flex items-center font-projeto font-bold text-mediumgray hover:text-almostblack md:ml-4 ">Carreers</button>
+                    <button class="m-1 my-3 flex items-center font-projeto font-bold text-mediumgray hover:text-almostblack md:ml-4 ">About</button>
                 </ul>
 
-                <div class=" flex flex-col w-full p-8" id="auth">
+                <!-- REGISTRO E LOGIN -->
+
+                <div class=" flex flex-col w-full p-8 md:flex-row 
+                md:w-max md:p-2 " id="auth">
                     <button class="font-projeto text-mediumgray text-sm hover:text-almostblack">Login</button>
-                    <button class="font-projeto text-mediumgray border border-mediumgray rounded-xl py-2 px-4 mt-4 text-sm hover:text-almostblack">Register</button>
+                    <button class="font-projeto text-mediumgray border border-mediumgray rounded-xl py-2 px-4 mt-4 text-sm hover:text-almostblack
+                    md:ml-4 md:my-2">Register</button>
                 </div>
             </div>
         </nav>
     </header>
+    <!-- / HEADER NAV & LOGIN -->
 
-    <main class="flex flex-col h-screen py-4">
+    <main class="  flex flex-col h-screen py-4
+    md:flex-row">
 
         <div class="flex w-full">
             <img src="../img/image-hero-mobile.png" class=" object-contain " alt="image-hero">
@@ -139,7 +181,6 @@
             </div>
         </div>
     </main>
-    <!-- <footer class="bg-slate-800 h-auto w-full">a</footer> -->
 </body>
 
 </html>
